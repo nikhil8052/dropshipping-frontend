e2803bbd13212e8b00bc97d0f28fbf5fe9fa5b96
Muhammad Zeshan <muhammad.zeshan@ropstam.com>
2024-08-28 17:18:44 +0500

    update name issue

--- src/pages/Dashboard/Coaches/NewCoach/NewCoach.jsx
+++ src/pages/Dashboard/Coaches/NewCoach/NewCoach.jsx
@@ -160,17 +160,13 @@ const NewCoach = () => {
         phoneNumber: Yup.string()
             .required('Phone number is required')
             .trim('Phone number cannot include leading or trailing spaces')
-            .test(
-                'is-valid-phone',
-                'Phone number must be in the format of belgium or netherlands i.e +32 or +31',
-                (value) => {
-                    if (!value) return false; // If value is undefined/null, it fails the test
-                    // Check for the valid patterns
-                    const belgiumPattern = '32';
-                    const netherlandsPattern = '31';
-                    return value.startsWith(belgiumPattern) || value.startsWith(netherlandsPattern);
-                }
-            )
+            .test('is-valid-phone', 'Phone number must be in the correct format country', (value) => {
+                if (!value) return false; // If value is undefined/null, it fails the test
+                // Check for the valid patterns
+                const belgiumPattern = '32';
+                const netherlandsPattern = '31';
+                return value.startsWith(belgiumPattern) || value.startsWith(netherlandsPattern);
+            })
             .strict(true), // ensures trimming is enforced during validation
 
         country: Yup.string().required('Country is required'),
@@ -381,8 +377,7 @@ const NewCoach = () => {
                                             name="phoneNumber"
                                             label="Phone Number"
                                             defaultCountry="nl" // Default to Belgium
-                                            countriesAllowed={['be', 'nl']} // Allow only Belgium and Netherlands
-                                            placeholder="+32-24-3611111"
+                                            countriesAllowed={values.country === 'Netherlands' ? ['nl'] : ['be']}
                                         />
                                     </Col>
                                     <Col md={6} xs={12}>
