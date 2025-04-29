9febf8a78caf8569d6b43df8763c2690b8950b48
nikhil8052 <nikhilkumarnk1142@gmail.com>
2025-04-29 16:16:19 +0530

    Latest code rename

--- src/pages/Dashboard/Courses-supabase/CourseThumbnail.jsx
+++ src/pages/Dashboard/Courses-supabase/CourseThumbnail.jsx
@@ -11,120 +11,121 @@ import { API_URL } from '../../../utils/apiUrl';
 import cross from '@icons/red-cross.svg';
 
 const CourseThumbnail = ({ value, onChange }) => {
-    const inputRef = useRef();
-    const [loadingThum, setLoadingThumb] = useState(false);
-    const [cropping, setCropping] = useState(false);
-    const [imageSrc, setImageSrc] = useState(null);
 
-    const handleFileChange = async (e) => {
-        const file = e.target.files[0];
-        if (!file?.type?.startsWith('image/')) {
-            toast.error('Please select a valid image file');
-            return;
-        }
-        const image = URL.createObjectURL(file);
-        setImageSrc(image);
-        setCropping(true);
-    };
+  console.log( value, " THUMB VALUE ")
+  const inputRef = useRef();
+  const [loadingThum, setLoadingThumb] = useState(false);
+  const [cropping, setCropping] = useState(false);
+  const [imageSrc, setImageSrc] = useState(null);
 
-    const handleCropComplete = async (croppedImage) => {
-        setLoadingThumb(true);
-        try {
-            const file = await getFileObjectFromBlobUrl(croppedImage, 'courseThumbnail.jpeg');
-            const formData = new FormData();
-            formData.append('files', file);
-            formData.append('name', file.name);
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    if (!file?.type?.startsWith('image/')) {
+      toast.error('Please select a valid image file');
+      return;
+    }
+    const image = URL.createObjectURL(file);
+    setImageSrc(image);
+    setCropping(true);
+  };
 
-            const mediaFile = await axiosWrapper('POST', API_URL.UPLOAD_MEDIA, formData, '', true);
-            onChange(mediaFile.data[0].path);
-        } catch (error) {
-            toast.error('Failed to upload thumbnail');
-        } finally {
-            setCropping(false);
-            setLoadingThumb(false);
-        }
-    };
+  const handleCropComplete = async (croppedImage) => {
+    setLoadingThumb(true);
+    try {
+      const file = await getFileObjectFromBlobUrl(croppedImage, 'courseThumbnail.jpeg');
+      const formData = new FormData();
+      formData.append('files', file);
+      formData.append('name', file.name);
 
-    const resetCropper = () => {
-        setCropping(false);
-        onChange('');
-        setImageSrc(null);
-        if (inputRef.current) inputRef.current.value = null;
-    };
-    const [showTranscriptEditor, setShowTranscriptEditor] = useState(false);
+      const mediaFile = await axiosWrapper('POST', API_URL.UPLOAD_MEDIA, formData, '', true);
+      onChange(mediaFile.data[0].path);
+    } catch (error) {
+      toast.error('Failed to upload thumbnail');
+    } finally {
+      setCropping(false);
+      setLoadingThumb(false);
+    }
+  };
 
-    return (
-        <div className="upload-form-section thumbnail-block">
-        <div className="upload-course-form">
-                <div className="thumbnail">
-                    {!value && <label>Course Thumbnail</label>}
-                    <input
-                        ref={inputRef}
-              accept=".jpg,.jpeg,.png"
-                        type="file"
-                        style={{ display: 'none' }}
-                        onChange={handleFileChange}
-                    />
+  const resetCropper = () => {
+    setCropping(false);
+    onChange('');
+    setImageSrc(null);
+    if (inputRef.current) inputRef.current.value = null;
+  };
+  const [showTranscriptEditor, setShowTranscriptEditor] = useState(false);
 
-                    {loadingThum ? (
-                        <Loading />
-                    ) : value ? (
-                        <div className="image-renderer">
-                            <span>Course Thumbnail</span>
-                            <img
-                                src={value}
-                                alt="Course thumbnail"
-                                style={{
-                                    borderRadius: '50%',
-                                    objectFit: 'cover',
-                                    width: '200px',
-                                    height: '128px'
-                                }}
-                            />
-                            <div className="align-self-start" style={{ marginLeft: 'auto' }}>
-                  <img
-                    src={cross}
-                    onClick={() => onChange('')}
-                    className="reset-image"
-                    alt="reset"
-                            </div>
-                        </div>
-                    ) : (
-                        <div className="image-preview">
-                            <img src={courseThumbnail} alt="thumbnail" />
-                            <div className="image-preview-text">
-                                <div>
-                                    <p>
-                      Upload your course Thumbnail here. <strong>Important guidelines: </strong> 
-                      1200x800 pixels or 12:8 Ratio. Supported format: <strong>.jpg, .jpeg, or .png</strong>
-                                    </p>
-                                </div>
-                                <Button
-                                    type="button"
-                                    className="upload-btn"
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        inputRef.current?.click();
-                                    }}
-                                >
-                    Upload Image{' '}
-                    <img className="mb-1" src={UploadSimple} alt="Upload Btn" />
-                                </Button>
-                            </div>
-                        </div>
-                    )}
+  return (
+    <div className="upload-form-section thumbnail-block">
+      <div className="upload-course-form">
+        <div className="thumbnail">
+          {!value && <label>Course Thumbnail</label>}
+          <input
+            ref={inputRef}
+            accept=".jpg,.jpeg,.png"
+            type="file"
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+          />
+          
+          {loadingThum ? <Loading /> : value ? (
+            <div className="image-renderer">
+              <span>Course Thumbnail</span>
+              <img
+                src={value}
+                alt="Course thumbnail"
+                style={{
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                  width: '200px',
+                  height: '128px'
+                }}
+              />
+              <div className="align-self-start" style={{ marginLeft: 'auto' }}>
+                <img
+                  src={cross}
+                  onClick={() => onChange('')}
+                  className="reset-image"
+                  alt="reset"
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="image-preview">
+              <img src={courseThumbnail} alt="thumbnail" />
+              <div className="image-preview-text">
+                <div>
+                  <p>
+                    Upload your course Thumbnail here. <strong>Important guidelines: </strong> 
+                    1200x800 pixels or 12:8 Ratio. Supported format: <strong>.jpg, .jpeg, or .png</strong>
+                  </p>
                 </div>
-
-                {cropping && (
-        <ImageCropper
-              imageSrc={imageSrc}
-              onCropComplete={handleCropComplete}
-              onCancel={resetCropper}
+                <Button
+                  type="button"
+                  className="upload-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    inputRef.current?.click();
+                  }}
+                >
+                  Upload Image{' '}
+                  <img className="mb-1" src={UploadSimple} alt="Upload Btn" />
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+        
+        {cropping && (
+          <ImageCropper
+            imageSrc={imageSrc}
+            onCropComplete={handleCropComplete}
+            onCancel={resetCropper}
           />
         )}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CourseThumbnail;
