8c300470a297d779bdf45e23cfd041e5ab15dc42
Nikita <sagmeticnikita@gmail.com>
2025-05-08 16:42:39 +0530

    nw

--- src/pages/Dashboard/Students-supabase/Students.jsx
+++ src/pages/Dashboard/Students-supabase/Students.jsx
@@ -879,7 +879,7 @@ const Students = () => {
                         zIndex: 1000,
                         backgroundColor: 'white',
                         boxShadow: '0 2px 10px rgba(0,0,0,0.2)',
-                        width: '',
+                        width: '250px',
                         borderRadius: '4px'
                     }}
                 >
