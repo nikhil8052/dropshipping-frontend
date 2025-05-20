733a8961f6be2f541a18672395dad0b947635d08
nikhil8052 <nikhilkumarnk1142@gmail.com>
2025-05-20 12:40:04 +0530

    update roadmap iframe link

--- src/pages/Dashboard/Roadmap/Roadmap.jsx
+++ src/pages/Dashboard/Roadmap/Roadmap.jsx
@@ -8,13 +8,13 @@ const Roadmap = () => {
             {roadMap && roadMap === 'ROAD_MAP_ONE' ? (
                 <iframe
                     style={{ border: '1px solid rgba(0, 0, 0, 0.1)', height: '100vh', width: '100%' }}
-                    src="https://embed.figma.com/proto/xEq9gZqwfZonQvFl4A3AKI/DSAX-Roadmap?node-id=66-841&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=111%3A940&show-proto-sidebar=0&embed-host=share"
+                    src="https://embed.figma.com/proto/meQPbSNlGpyYkPRM7MxXWA/DSAX-Roadmap-2.0?node-id=133-1490&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=111%3A940&show-proto-sidebar=0&embed-host=share"
                     allowFullScreen
                 ></iframe>
             ) : (
                 <iframe
                     style={{ border: '1px solid rgba(0, 0, 0, 0.1)', height: '100vh', width: '100%' }}
-                    src="https://embed.figma.com/proto/xEq9gZqwfZonQvFl4A3AKI/DSAX-Roadmap?node-id=66-841&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=111%3A940&show-proto-sidebar=0&embed-host=share"
+                    src="https://embed.figma.com/proto/meQPbSNlGpyYkPRM7MxXWA/DSAX-Roadmap-2.0?node-id=133-1490&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=111%3A940&show-proto-sidebar=0&embed-host=share"
                     allowFullScreen
                 ></iframe>
             )}
